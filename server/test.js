f102633cc92999dedf406e0ed5dd3ad11ca36c2b
Muhammad Saeed <62692034+me-saeed@users.noreply.github.com>
2022-10-20 18:09:43 +0500

    changes

--- server/test.js
+++ server/test.js
@@ -19,25 +19,49 @@
 //     console.error(error);
 //   });
 
-const axios = require('axios');
+const axios = require("axios");
 
 const options = {
-  method: 'GET',
-  url: 'https://mashvisor-api.p.rapidapi.com/city/listings',
+  method: "GET",
+  url: "https://mashvisor-api.p.rapidapi.com/property",
   params: {
-    page: '1',
-    state: 'CA',
+    address: "1421 SW 18th Street",
+    city: "Boca Raton",
+    state: "Fl",
+    zip_code: "33486",
   },
   headers: {
-    'X-RapidAPI-Key': '812472ad0emshd27325541276399p156f59jsn442b6fe2a9d6',
-    'X-RapidAPI-Host': 'mashvisor-api.p.rapidapi.com',
+    "X-RapidAPI-Key": "812472ad0emshd27325541276399p156f59jsn442b6fe2a9d6",
+    "X-RapidAPI-Host": "mashvisor-api.p.rapidapi.com",
   },
 };
 
 axios
   .request(options)
   .then(function (response) {
-    console.log(response.data.content);
+    //// console.log(response.data);
+
+    console.log(response.data);
+
+    // axios
+    //   .request({
+    //     method: "GET",
+    //     url: `https://mashvisor-api.p.rapidapi.com/airbnb-property/${response.data.content.id}`,
+    //     params: { state: "Fl" },
+    //     headers: {
+    //       "X-RapidAPI-Key":
+    //         "812472ad0emshd27325541276399p156f59jsn442b6fe2a9d6",
+    //       "X-RapidAPI-Host": "mashvisor-api.p.rapidapi.com",
+    //     },
+    //   })
+    //   .then(function (response) {
+    //     //// console.log(response.data);
+
+    //     console.log(response.data);
+    //   })
+    //   .catch(function (error) {
+    //     console.error(error);
+    //   });
   })
   .catch(function (error) {
     console.error(error);
