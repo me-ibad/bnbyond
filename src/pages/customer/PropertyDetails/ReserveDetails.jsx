f0cf76eebbdbbaf4c7ce7141bd1488576832bb77
Zeeshan Yousaf <zeeshanento34@gmail.com>
2022-12-21 18:18:32 +0500

    reserveDetailUpdate

--- src/pages/customer/PropertyDetails/ReserveDetails.jsx
+++ src/pages/customer/PropertyDetails/ReserveDetails.jsx
@@ -6,10 +6,14 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { useState } from "react";
+import { ImageEndPoint } from "config/config";
+import Rating from '@mui/material/Rating';
+import { localStorageData, Logout } from "services/auth/localStorageData";
+
 
 function ReserveDetails() {
   const navigate = useNavigate();
-
+  const [value, setValue] = React.useState(5);
   const location = useLocation();
   const { state } = location;
   console.log(state, "my state");
@@ -32,13 +36,22 @@ function ReserveDetails() {
       setCount(num);
     }
   }
-  console.log(state.propertyDetail,"propertyDetail")
+  console.log(state.propertyDetail, "propertyDetail");
+const onClickContinue=()=>{
 
+  if(localStorageData("_id")){
+ alert("button Clicked")
+  }
+
+else{
+  navigate('/auth/signin')
+}
+}
   return (
     <>
       <Container maxWidth="xl">
-        <div>
-          <div>
+        <div className=" reserveDetailWrapper flex mt-2">
+          <div className=" w-6/12">
             <div className="h-24 flex item-center">
               <div className="flex items-center ">
                 <i class="fas fa-angle-left"></i>
@@ -120,9 +133,32 @@ function ReserveDetails() {
                 </button>
               </div>
             </div>
+            <div className="btn-continue w-full flex justify-center">
+           
+              <button className="w-80 py-2 bg-red-500 text-white rounded"
+              onClick={onClickContinue}
+              >
+            Continue
+                </button>
+            </div>
           </div>
-          <div>
+{/* Image Areas */}
+          <div className="flex mt-20">
 
+            <div className="reserveImage  p-4">
+              <img
+                src={ImageEndPoint + state.propertyDetail.pics[0]}
+                className="w-80 object-cover rounded "
+                alt=""
+              />
+            </div>
+            <div className=" flex items-center h-48">
+            <div className="propertyName ">
+              <p>{state.propertyDetail.title}</p>
+     <p> <Rating name="read-only" value={value} readOnly /></p>
+     <p> points $400</p>
+     </div>
+            </div>
           </div>
         </div>
       </Container>
